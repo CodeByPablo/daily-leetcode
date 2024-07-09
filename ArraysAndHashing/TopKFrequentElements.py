1965c8918fd9f9c760db8d09f8d69e1c28a8426e
Pablo Guzman <pmartinez08@alumnos.uaq.mx>
2024-07-08 20:54:48 -0600

    initial solution

--- ArraysAndHashing/TopKFrequentElements.py
+++ ArraysAndHashing/TopKFrequentElements.py
@@ -2,7 +2,21 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+        # initial solution
+        seen = {}
+        top_frequent = []
+
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                seen[nums[i]] = 1 + seen.get(nums[i], 0)
+            else:
+                seen[nums[i]] = 1
+
+        for n in range(k):
+            max_key = max(seen, key=seen.get)
+            top_frequent.append(max_key)
+            seen.pop(max_key)
+        return top_frequent
 
 solution = Solution()
-print(solution.topKFrequent())
+print(solution.topKFrequent([1,1,1,2,2,3,4,4,4,4,4], 3))

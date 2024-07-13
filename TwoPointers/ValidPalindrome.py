421459738a0100d995ef53947044eb7d1d75eb3a
Pablo Guzman <pmartinez08@alumnos.uaq.mx>
2024-07-12 22:22:09 -0600

    initial solution

--- TwoPointers/ValidPalindrome.py
+++ TwoPointers/ValidPalindrome.py
@@ -1,5 +1,19 @@
-# class Solution:
-#     def isPalindrome(self, s:str) -> bool:
 
-# solution = Solution()
-# print(solution.isPalindrome("asdasdas"))
+class Solution:
+    def isPalindrome(self, s:str) -> bool:
+        p1 = 0
+        p2 = len(s) - 1
+
+        while p2 >= p1 and s != " ":
+            while not s[p1].isalpha():
+                p1 += 1
+            while not s[p2].isalpha():
+                p2 -= 1
+            if s[p1].lower() != s[p2].lower():
+                return False
+            p1 += 1
+            p2 -= 1
+        return True
+
+solution = Solution()
+print(solution.isPalindrome("A man, a plan, a canal: Panama"))

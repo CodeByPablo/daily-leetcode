1f0a4763e8bedeb16274117f2cb9ede4a9afa963
Pablo Guzman <pmartinez08@alumnos.uaq.mx>
2024-07-15 13:27:25 -0600

    isalpha solution

--- TwoPointers/ValidPalindrome.py
+++ TwoPointers/ValidPalindrome.py
@@ -1,19 +1,14 @@
 
 class Solution:
     def isPalindrome(self, s:str) -> bool:
-        p1 = 0
-        p2 = len(s) - 1
+        # isalpha function using extra memory
+        new_str = ""
 
-        while p2 >= p1 and s != " ":
-            while not s[p1].isalpha():
-                p1 += 1
-            while not s[p2].isalpha():
-                p2 -= 1
-            if s[p1].lower() != s[p2].lower():
-                return False
-            p1 += 1
-            p2 -= 1
-        return True
+        for c in s:
+            if c.isalpha():
+                new_str += c.lower()
+        
+        return new_str == new_str[::-1]
 
 solution = Solution()
 print(solution.isPalindrome("A man, a plan, a canal: Panama"))
